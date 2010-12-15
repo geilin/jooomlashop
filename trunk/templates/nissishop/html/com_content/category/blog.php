2becfedfccda217d6a0032ec8ec4eadaffb0a88a
phplaw <phplaw@34c70645-75e1-d5d2-b079-1858407ea437>
2010-12-15 18:11:50 +0000

    update source

--- trunk/templates/nissishop/html/com_content/category/blog.php
+++ trunk/templates/nissishop/html/com_content/category/blog.php
@@ -16,63 +16,62 @@ require_once(JPATH_SITE.DS.'templates'.DS.'thoitrang'.DS.'tpl_function.php');
 
 <!-- component_content -->
 <div id="component_content" class="clearfix">
-
-	<div class="block_left_ct" class="clearfix">
-	<?php if ($this->params->def('show_description', 1) || $this->params->def('show_description_image', 1))
-	{
-	?>
-		<div class="cat">
-			<?php if ($this->params->get('show_description_image') && $this->category->image) : ?>
-				<img src="<?php echo $this->baseurl . '/' . $cparams->get('image_path') . '/'. $this->category->image;?>" align="<?php echo $this->category->image_position;?>" hspace="6" alt="" />
-			<?php endif; ?>
-			<?php if ($this->params->get('show_description') && $this->category->description) echo $this->category->description; ?>
-		</div><!-- /cat -->
-	<?php 
-	} 
-		if ($this->params->get('num_leading_articles')) {
-			for ($i = $this->pagination->limitstart; $i < ($this->pagination->limitstart + $this->params->get('num_leading_articles')); $i++) 
-			{  			
-				if ($i >= $this->total) break;
-				
-				$this->item =& $this->getItem($i, $this->params);
-				echo $this->loadTemplate('item');
-			}
-		}
-		else
+	<div class="component_content_wrapper clearfix">
+		<?php if ($this->params->def('show_description', 1) || $this->params->def('show_description_image', 1))
 		{
-			$i = $this->pagination->limitstart;
-		}
-
-	$startIntroArticles = $this->pagination->limitstart + $this->params->get('num_leading_articles');
-	$numIntroArticles = $startIntroArticles + $this->params->get('num_intro_articles');
-	if (($numIntroArticles != $startIntroArticles) && ($i < $this->total)) {
-		$divider = '';
-		for ($z = 0; $z < $this->params->get('num_columns'); $z ++)
-		{
-			$divider = ($z > 0) ? " column_separator" :  ''; 
-			for ($y = 0; $y < ($this->params->get('num_intro_articles') / $this->params->get('num_columns')); $y ++)
-			{
-				if ($i < $this->total && $i < ($numIntroArticles)){
+		?>
+			<div class="cat">
+				<?php if ($this->params->get('show_description_image') && $this->category->image) : ?>
+					<img src="<?php echo $this->baseurl . '/' . $cparams->get('image_path') . '/'. $this->category->image;?>" align="<?php echo $this->category->image_position;?>" hspace="6" alt="" />
+				<?php endif; ?>
+				<?php if ($this->params->get('show_description') && $this->category->description) echo $this->category->description; ?>
+			</div><!-- /cat -->
+		<?php 
+		} 
+			if ($this->params->get('num_leading_articles')) {
+				for ($i = $this->pagination->limitstart; $i < ($this->pagination->limitstart + $this->params->get('num_leading_articles')); $i++) 
+				{  			
+					if ($i >= $this->total) break;
+					
 					$this->item =& $this->getItem($i, $this->params);
 					echo $this->loadTemplate('item');
-					$i ++;
+				}
+			}
+			else
+			{
+				$i = $this->pagination->limitstart;
+			}
+
+		$startIntroArticles = $this->pagination->limitstart + $this->params->get('num_leading_articles');
+		$numIntroArticles = $startIntroArticles + $this->params->get('num_intro_articles');
+		if (($numIntroArticles != $startIntroArticles) && ($i < $this->total)) {
+			$divider = '';
+			for ($z = 0; $z < $this->params->get('num_columns'); $z ++)
+			{
+				$divider = ($z > 0) ? " column_separator" :  ''; 
+				for ($y = 0; $y < ($this->params->get('num_intro_articles') / $this->params->get('num_columns')); $y ++)
+				{
+					if ($i < $this->total && $i < ($numIntroArticles)){
+						$this->item =& $this->getItem($i, $this->params);
+						echo $this->loadTemplate('item');
+						$i ++;
+					}
 				}
 			}
 		}
-	}
-	?>
-		<?php if ($this->params->get('num_links') && ($i < $this->total)) : ?>
-			<div class="cb blog_more<?php echo $this->params->get('pageclass_sfx');?>">
-			<?php
-				$this->links = array_splice($this->items, $i - $this->pagination->limitstart);
-				echo $this->loadTemplate('links');
-			?>
-			</div>
-		<?php endif; ?>
-		<?php if ($this->params->get('show_pagination')): ?>
-			<div class="h_10"></div><div class="paging cb"><?php echo $this->pagination->getPagesLinks(); ?></div>
-		<?php endif; ?>
-	</div>
+		?>
+			<?php if ($this->params->get('num_links') && ($i < $this->total)) : ?>
+				<div class="cb blog_more<?php echo $this->params->get('pageclass_sfx');?>">
+				<?php
+					$this->links = array_splice($this->items, $i - $this->pagination->limitstart);
+					echo $this->loadTemplate('links');
+				?>
+				</div>
+			<?php endif; ?>
+			<?php if ($this->params->get('show_pagination')): ?>
+				<?php echo $this->pagination->getPagesLinks(); ?>
+			<?php endif; ?>
+		</div>
 
 </div><!-- /component_content -->
 <div id="component_footer"><span></span></div>
