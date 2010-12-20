9f7edcc90b61295942653d480dd17cee22b7a5e1
phplaw <phplaw@34c70645-75e1-d5d2-b079-1858407ea437>
2010-12-20 20:55:41 +0000

    update module

--- modules/mod_site_statistic/tmpl/default.php
+++ modules/mod_site_statistic/tmpl/default.php
@@ -3,10 +3,10 @@
 defined('_JEXEC') or die('Restricted access'); 
 ?>
 <style type="text/css">
-.module_content_wrapper { padding:10px 10px 0; line-height:25px;  }
-.site_statistic_line span { font-weight:bold; }
+
+.site_statistic_line span { font-weight:bold; line-height:25px;  }
 </style>
-<div class="module_content_wrapper">
+
 	<p class="site_statistic_line">Số người đang online: <span><?php echo $totalonline; ?></span></p>
 	<p class="site_statistic_line">Tổng số lượt truy cập: <span><?php echo $total_visited; ?></span></p>
-</div>
+
