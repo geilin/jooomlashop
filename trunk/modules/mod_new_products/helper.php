12502c35bf93d6efeb264b6532a24d333ee2152d
phplaw <phplaw@34c70645-75e1-d5d2-b079-1858407ea437>
2010-12-20 20:55:14 +0000

    update helper module new product

--- trunk/modules/mod_new_products/helper.php
+++ trunk/modules/mod_new_products/helper.php
@@ -1,13 +1,4 @@
 <?php
-/**
-* @version		1.0  - 	Joomla 1.5.x
-* @package		Module Cat Menu Products
-* @copyright	Wampvn Group
-* @license		GNU/GPL
-* @website          http://wampvn.com
-* @description    helper.
-*/
-
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
@@ -15,15 +6,17 @@ class modNewProductsHelper
 {
 	function getNewProducts($params)
 	{		
-		$db			=& JFactory::getDBO();				
+		$db	=& JFactory::getDBO();				
 		
-		$limit = (int) trim($params->get('limit',"8"));
+		$limit = (int) trim($params->get('limit', 8));
 		
-		$query =  "SELECT p.id, p.name, p.currency, p.saleprice, p.thumbnail"
-				  . " FROM #__w_products as p"
-				  . " WHERE p.published = 1 "	
-				  . " ORDER BY  p.date DESC"
-				  . " LIMIT 0,".$limit;		
+		$query =  'SELECT p.id, p.name, p.currency,'
+				  . ' p.saleprice, i.filename'
+				  . ' FROM #__w_products AS p'
+				  . ' LEFT JOIN #__w_images AS i ON p.image = i.id'
+				  . ' WHERE p.published = 1'	
+				  . ' ORDER BY  p.date DESC'
+				  . ' LIMIT 0,'.$limit;		
 		$db->setQuery($query);
 		$rows = $db->loadObjectList();
 		
@@ -31,21 +24,9 @@ class modNewProductsHelper
 			$row =& $rows[$i];
 			$title = JFilterOutput::stringURLSafe($row->name);
 			$title = str_replace(' ', '-', strtolower($title));
-			$rows[$i]->link = JRoute::_('index.php?option=com_products' .
-			'&view=detail&id=' . $row->id. ':'. $title);
-			$rows[$i]->thumbnail = 'phones/'. $rows[$i]->thumbnail;			
-		}
-		
+			$rows[$i]->link = JRoute::_('index.php?option=com_products' . '&view=detail&id=' . $row->id. ':'. $title);		
+		}		
 		return $rows;
 	}	
-
-	
-	function getImageDefault($proid){
-		$db     =& JFactory::getDBO();	
-		$query  = "SELECT filename FROM #__w_images WHERE proid=" .(int)$proid . " AND published =1 AND isdefault =1 LIMIT 1";
-        return $db->GetOne($query);
-	}
-	
-	
 }
 
